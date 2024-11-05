8ef243476a8623271ea1747698339a18284f7467
Randall Leon Madriz <rleonm99@gmail.com>
2024-11-05 14:14:54 -0600

    add adder project

---
+++ Adder_project/Adder_project.srcs/sources_1/new/Adder.sv
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/03/2024 02:05:16 PM
+// Design Name: 
+// Module Name: Adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Adder
+    #( parameter N = 4)
+    (
+    input logic [N - 1: 0] X, Y,
+    input logic C_in,
+    output logic C_out,
+    output logic [N - 1: 0] S,
+    output logic overflow
+    );
+    
+    assign {C_out,S} = X + Y + C_in;
+    assign overflow = (X[N-1] & Y[N-1] & ~S[N-1]) | (~X[N-1] & ~Y[N-1] & S[N-1]);
+endmodule
